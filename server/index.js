860e8ade42ac9d7d5e6316192565ff68fcab67b9
Kamal-Sin <kamal364997@gmail.com>
2025-09-19 03:25:28 +0530

    Collaboard

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
-const { Socket } = require("dgram");
-const express=require("express");
-const app=express();
-const http=require("http");
-const { connect } = require("http2");
-const { disconnect } = require("process");
-const {Server}=require("socket.io")
+import { Socket } from "dgram";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
 const server=http.createServer(app)
 const io=new Server(server);
 
